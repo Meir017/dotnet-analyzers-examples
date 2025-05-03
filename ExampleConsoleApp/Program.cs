dec34110957ddac3d7a084643f1e26fba3c7bd25
Meir Blachman <meirblachman@gmail.com>
2025-05-03 23:09:51 +0300

    basic library using analyzer

---
+++ ExampleConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace ExampleConsoleApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine(nameof(MethodWithManyParameters));
+    }
+
+    public void MethodWithManyParameters(int a, int b, int c, int d, int e)
+    {
+        Console.WriteLine(a + b + c + d + e);
+    }
+}
